6cbde8211d65d35da397b6c05a90cd6f1a74201a
cheitovilla <33737589+cheitovilla@users.noreply.github.com>
2018-02-09 20:30:05 -0500

    camera ready

--- MarioBros/Assets/Scripts/Movimiento.cs
+++ MarioBros/Assets/Scripts/Movimiento.cs
@@ -16,67 +16,80 @@ public class Movimiento : MonoBehaviour {
 
     public bool down;
 
+    
+
+
     Animator animator;
 
     void Awake(){
        animator = GetComponent <Animator>();
+       
     }
     // Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
 
         float inputX = Input.GetAxis("Horizontal");
-        if (!down){
-            if (inputX>0)
-            {
-                movX = transform.position.x + (inputX*velX);
-                transform.position = new Vector3 (movX, transform.position.y, 0);
-                transform.localScale = new Vector3 (1, 1, 1);
-            }
-
-            if (inputX<0)
+        if (!down)
+        {
+            if (inputX > 0)
             {
                 movX = transform.position.x + (inputX * velX);
-                transform.position = new Vector3 (movX, transform.position.y, 0);
-                transform.localScale = new Vector3 (-1, 1, 1);
+                transform.position = new Vector3(movX, transform.position.y, 0);
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if (inputX < 0)
+            {
+                movX = transform.position.x + (inputX * velX);
+                transform.position = new Vector3(movX, transform.position.y, 0);
+                transform.localScale = new Vector3(-1, 1, 1);
             }
         }
 
-        if (inputX !=0 && ensuelo){
-            animator.SetFloat ("velX",1);
-            }
-            else {
-            animator.SetFloat ("velX",0);
-            }
-        
-
-        ensuelo = Physics2D.OverlapCircle (pie.position, radiopie, suelo);
-        if (ensuelo ){
-            animator.SetBool("ensuelo",true);
-             if (Input.GetKeyDown(KeyCode.Space) && !down)
+        if (inputX != 0 && ensuelo)
         {
-            GetComponent<Rigidbody2D>().AddForce (new Vector2(0, salto));
-            animator.SetBool ("ensuelo",false);
+            animator.SetFloat("velX", 1);
         }
+        else
+        {
+            animator.SetFloat("velX", 0);
+        }
+
+
+        ensuelo = Physics2D.OverlapCircle(pie.position, radiopie, suelo);
+        if (ensuelo)
+        {
+            animator.SetBool("ensuelo", true);
+            if (Input.GetKeyDown(KeyCode.Space) && !down)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, salto));
+                animator.SetBool("ensuelo", false);
             }
-            else{
-                animator.SetBool("ensuelo",false);
-            }
+        }
+        else
+        {
+            animator.SetBool("ensuelo", false);
+        }
         //Debug.Log (ensuelo);
 
-       
 
-        if (ensuelo && Input.GetKey (KeyCode.DownArrow)) {
-            animator.SetBool("down",true);
+
+        if (ensuelo && Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetBool("down", true);
             down = true;
         }
-        else {
-            animator.SetBool("down",false);
+        else
+        {
+            animator.SetBool("down", false);
             down = false;
         }
-	}
+
+    }
 }
