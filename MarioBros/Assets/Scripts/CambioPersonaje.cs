60bd32abb776589db0554a316a1b79003f73f7b1
cheitovilla <33737589+cheitovilla@users.noreply.github.com>
2018-02-22 20:54:46 -0500

    mario 2

--- MarioBros/Assets/Scripts/CambioPersonaje.cs
+++ MarioBros/Assets/Scripts/CambioPersonaje.cs
@@ -14,44 +14,46 @@ public class CambioPersonaje : MonoBehaviour {
 	}
 	
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (player1.activeSelf == true)
+    //void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    if (player1.activeSelf == true)
+    //    {
+    //        Debug.Log("Hola");
+    //        player2.transform.position = player1.transform.position;
+    //        player1.SetActive(false);
+    //        player2.SetActive(true);
+    //        canchange = false;
+    //    }
+
+    //   else if (player2.activeSelf == true)
+    //    {
+    //        Debug.Log("Hola");
+    //        player1.transform.position = player2.transform.position;
+    //        player2.SetActive(false);
+    //        player1.SetActive(true);
+    //        canchange = true;
+    //    }
+
+    //}
+
+	// Update is called once per frame
+	void Update () {
+        if (player1.transform.position.x > 43 && canchange)
         {
-            Debug.Log("Hola");
+            Debug.Log("Cambio a 2");
             player2.transform.position = player1.transform.position;
             player1.SetActive(false);
             player2.SetActive(true);
             canchange = false;
         }
-
-       else if (player2.activeSelf == true)
+        if (!canchange && player2.transform.position.x < 43)
         {
-            Debug.Log("Hola");
+            Debug.Log("Cambio a 1");
             player1.transform.position = player2.transform.position;
             player2.SetActive(false);
             player1.SetActive(true);
             canchange = true;
         }
-
-    }
-
-	// Update is called once per frame
-	void Update () {
-        //if (player1.transform.position.x>43 && canchange)
-        //{
-        //    player2.transform.position = player1.transform.position;
-        //    player1.SetActive(false);
-        //    player2.SetActive(true);
-        //    canchange = false;
-        //}
-        //if (!canchange && player2.transform.position.x < 10)
-        //{
-        //    player1.transform.position = player2.transform.position;
-        //    player2.SetActive(false);
-        //    player1.SetActive(true);
-        //    canchange = true;
-        //}
 
     }
 }
